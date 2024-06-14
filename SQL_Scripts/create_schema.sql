7ab9031aec4ad29e28175104bd32f1713b087301
miranda <mirandalucas@id.uff.br>
2024-06-13 23:27:29 -0300

    att schema

--- SQL_Scripts/create_schema.sql
+++ SQL_Scripts/create_schema.sql
@@ -2,77 +2,77 @@ CREATE DATABASE IF NOT EXISTS escola;
 USE escola;
 
 CREATE TABLE Cidade (
-    Codigo INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(50) NOT NULL
+    codigo INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Pessoa (
-    Codigo INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(50) NOT NULL,
-    Telefone VARCHAR(15) NOT NULL,
-    Codigo_Cidade INT NOT NULL,
-    FOREIGN KEY (Codigo_Cidade) REFERENCES Cidade(Codigo)
+    codigo INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    codigo_cidade INT NOT NULL,
+    FOREIGN KEY (codigo_cidade) REFERENCES Cidade(codigo)
 );
 
 CREATE TABLE Professor (
-    Codigo INT PRIMARY KEY,
-    RG VARCHAR(20) NOT NULL,
-    CPF VARCHAR(20) NOT NULL,
-    Titulacao VARCHAR(50) NOT NULL,
-    FOREIGN KEY (Codigo) REFERENCES Pessoa(Codigo)
+    codigo INT PRIMARY KEY,
+    rg VARCHAR(20) NOT NULL,
+    cpf VARCHAR(20) NOT NULL,
+    titulacao VARCHAR(50) NOT NULL,
+    FOREIGN KEY (codigo) REFERENCES Pessoa(codigo)
 );
 
 CREATE TABLE Escola (
-    Codigo INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(50) NOT NULL,
-    Codigo_Diretor INT NOT NULL,
-    Codigo_Cidade INT NOT NULL,
-    FOREIGN KEY (Codigo_Diretor) REFERENCES Professor(Codigo),
-    FOREIGN KEY (Codigo_Cidade) REFERENCES Cidade(Codigo)
+    codigo INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    codigo_diretor INT NOT NULL,
+    codigo_cidade INT NOT NULL,
+    FOREIGN KEY (codigo_diretor) REFERENCES Professor(codigo),
+    FOREIGN KEY (codigo_cidade) REFERENCES Cidade(codigo)
 );
 
 CREATE TABLE Disciplina (
-    Codigo INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(50) NOT NULL
+    codigo INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Turma (
-    Codigo INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(50) NOT NULL,
-    Codigo_Escola INT NOT NULL,
-    FOREIGN KEY (Codigo_Escola) REFERENCES Escola(Codigo)
+    codigo INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    codigo_escola INT NOT NULL,
+    FOREIGN KEY (codigo_escola) REFERENCES Escola(codigo)
 );
 
 CREATE TABLE Aluno (
-    Codigo INT PRIMARY KEY,
-    Matricula_Aluno INT NOT NULL,
-    Data_Nascimento DATE NOT NULL,
-    Codigo_Turma INT NOT NULL,
-    FOREIGN KEY (Codigo) REFERENCES Pessoa(Codigo),
-    FOREIGN KEY (Codigo_Turma) REFERENCES Turma(Codigo)
+    codigo INT PRIMARY KEY,
+    matricula_aluno INT NOT NULL,
+    data_nascimento DATE NOT NULL,
+    codigo_turma INT NOT NULL,
+    FOREIGN KEY (codigo) REFERENCES Pessoa(codigo),
+    FOREIGN KEY (codigo_turma) REFERENCES Turma(codigo)
 );
 
 CREATE TABLE Contato (
-    Nome VARCHAR(50) NOT NULL,
-    Telefone VARCHAR(15) NOT NULL,
-    Codigo_Aluno INT NOT NULL,
-    PRIMARY KEY (Nome, Codigo_Aluno),
-    FOREIGN KEY (Codigo_Aluno) REFERENCES Aluno(Codigo)
+    nome VARCHAR(50) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    codigo_aluno INT NOT NULL,
+    PRIMARY KEY (nome, codigo_aluno),
+    FOREIGN KEY (codigo_aluno) REFERENCES Aluno(codigo)
 );
 
 CREATE TABLE Ministra (
-    Codigo_Professor INT NOT NULL,
-    Codigo_Disciplina INT NOT NULL,
-    PRIMARY KEY (Codigo_Professor, Codigo_Disciplina),
-    FOREIGN KEY (Codigo_Professor) REFERENCES Professor(Codigo),
-    FOREIGN KEY (Codigo_Disciplina) REFERENCES Disciplina(Codigo)
+    codigo_professor INT NOT NULL,
+    codigo_disciplina INT NOT NULL,
+    PRIMARY KEY (codigo_professor, codigo_disciplina),
+    FOREIGN KEY (codigo_professor) REFERENCES Professor(codigo),
+    FOREIGN KEY (codigo_disciplina) REFERENCES Disciplina(codigo)
 );
 
 CREATE TABLE Dar_Aula (
-    Codigo_Professor INT NOT NULL,
-    Codigo_Disciplina INT NOT NULL,
-    Codigo_Turma INT NOT NULL,
-    PRIMARY KEY (Codigo_Professor, Codigo_Disciplina, Codigo_Turma),
-    FOREIGN KEY (Codigo_Professor, Codigo_Disciplina) REFERENCES Ministra(Codigo_Professor, Codigo_Disciplina),
-    FOREIGN KEY (Codigo_Turma) REFERENCES Turma(Codigo)
+    codigo_professor INT NOT NULL,
+    codigo_disciplina INT NOT NULL,
+    codigo_turma INT NOT NULL,
+    PRIMARY KEY (codigo_professor, codigo_disciplina, codigo_turma),
+    FOREIGN KEY (codigo_professor, codigo_disciplina) REFERENCES Ministra(codigo_professor, codigo_disciplina),
+    FOREIGN KEY (codigo_turma) REFERENCES Turma(codigo)
 );
