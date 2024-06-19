73665b5c9f14dd7e847715474635762e054ba593
miranda <mirandalucas@id.uff.br>
2024-06-19 18:32:12 -0300

    tabelas c letra minuscula

--- SQL_Scripts/create_schema.sql
+++ SQL_Scripts/create_schema.sql
@@ -1,78 +1,78 @@
 CREATE DATABASE IF NOT EXISTS escola;
 USE escola;
 
-CREATE TABLE Cidade (
+CREATE TABLE cidade (
     codigo INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Pessoa (
+CREATE TABLE pessoa (
     codigo INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     telefone VARCHAR(15) NOT NULL,
     codigo_cidade INT NOT NULL,
-    FOREIGN KEY (codigo_cidade) REFERENCES Cidade(codigo)
+    FOREIGN KEY (codigo_cidade) REFERENCES cidade(codigo)
 );
 
-CREATE TABLE Professor (
+CREATE TABLE professor (
     codigo INT PRIMARY KEY,
     rg VARCHAR(20) NOT NULL,
     cpf VARCHAR(20) NOT NULL,
     titulacao VARCHAR(50) NOT NULL,
-    FOREIGN KEY (codigo) REFERENCES Pessoa(codigo)
+    FOREIGN KEY (codigo) REFERENCES pessoa(codigo)
 );
 
-CREATE TABLE Escola (
+CREATE TABLE escola (
     codigo INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     codigo_diretor INT NOT NULL,
     codigo_cidade INT NOT NULL,
-    FOREIGN KEY (codigo_diretor) REFERENCES Professor(codigo),
-    FOREIGN KEY (codigo_cidade) REFERENCES Cidade(codigo)
+    FOREIGN KEY (codigo_diretor) REFERENCES professor(codigo),
+    FOREIGN KEY (codigo_cidade) REFERENCES cidade(codigo)
 );
 
-CREATE TABLE Disciplina (
+CREATE TABLE disciplina (
     codigo INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE Turma (
+CREATE TABLE turma (
     codigo INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     codigo_escola INT NOT NULL,
-    FOREIGN KEY (codigo_escola) REFERENCES Escola(codigo)
+    FOREIGN KEY (codigo_escola) REFERENCES escola(codigo)
 );
 
-CREATE TABLE Aluno (
+CREATE TABLE aluno (
     codigo INT PRIMARY KEY,
     matricula_aluno INT NOT NULL,
     data_nascimento DATE NOT NULL,
     codigo_turma INT NOT NULL,
-    FOREIGN KEY (codigo) REFERENCES Pessoa(codigo),
-    FOREIGN KEY (codigo_turma) REFERENCES Turma(codigo)
+    FOREIGN KEY (codigo) REFERENCES pessoa(codigo),
+    FOREIGN KEY (codigo_turma) REFERENCES turma(codigo)
 );
 
-CREATE TABLE Contato (
+CREATE TABLE contato (
     nome VARCHAR(50) NOT NULL,
     telefone VARCHAR(15) NOT NULL,
     codigo_aluno INT NOT NULL,
     PRIMARY KEY (nome, codigo_aluno),
-    FOREIGN KEY (codigo_aluno) REFERENCES Aluno(codigo)
+    FOREIGN KEY (codigo_aluno) REFERENCES aluno(codigo)
 );
 
-CREATE TABLE Ministra (
+CREATE TABLE ministra (
     codigo_professor INT NOT NULL,
     codigo_disciplina INT NOT NULL,
     PRIMARY KEY (codigo_professor, codigo_disciplina),
-    FOREIGN KEY (codigo_professor) REFERENCES Professor(codigo),
-    FOREIGN KEY (codigo_disciplina) REFERENCES Disciplina(codigo)
+    FOREIGN KEY (codigo_professor) REFERENCES professor(codigo),
+    FOREIGN KEY (codigo_disciplina) REFERENCES disciplina(codigo)
 );
 
-CREATE TABLE Dar_Aula (
+CREATE TABLE dar_aula (
     codigo_professor INT NOT NULL,
     codigo_disciplina INT NOT NULL,
     codigo_turma INT NOT NULL,
     PRIMARY KEY (codigo_professor, codigo_disciplina, codigo_turma),
-    FOREIGN KEY (codigo_professor, codigo_disciplina) REFERENCES Ministra(codigo_professor, codigo_disciplina),
-    FOREIGN KEY (codigo_turma) REFERENCES Turma(codigo)
+    FOREIGN KEY (codigo_professor, codigo_disciplina) REFERENCES ministra(codigo_professor, codigo_disciplina),
+    FOREIGN KEY (codigo_turma) REFERENCES turma(codigo)
 );
--- SQL_Scripts/populate_tables.sql
+++ SQL_Scripts/populate_tables.sql
@@ -1,13 +1,13 @@
 USE escola;
 
-INSERT INTO Cidade (Nome) VALUES
+INSERT INTO cidade (nome) VALUES
     ('São Paulo'),
     ('Rio de Janeiro'),
     ('Belo Horizonte'),
     ('Salvador'),
     ('Brasília');
 
-INSERT INTO Pessoa (Nome, Telefone, Codigo_Cidade) VALUES
+INSERT INTO pessoa (nome, telefone, codigo_cidade) VALUES
 	-- SP
     ('João Silva', '(11) 9999-8888', 1), -- Aluno
     ('Paula Costa', '(11) 9444-3333', 1),
@@ -23,29 +23,29 @@ INSERT INTO Pessoa (Nome, Telefone, Codigo_Cidade) VALUES
     -- DF
     ('Pedro Santos', '(61) 9555-4444', 5); -- professor
 
-INSERT INTO Professor (Codigo, RG, CPF, Titulacao) VALUES
+INSERT INTO professor (codigo, rg, cpf, titulacao) VALUES
     (5, '12345678-9', '987654321-00', 'Doutorado'), -- professor
     (4, '66666669-0', '696969694-21', 'Doutorado'), -- diretor
     (8, '12313456-1', '341241245-23', 'Mestrado'), -- diretor
     (9, '32314145-4', '312874543-99', 'Mestrado'); -- professor
 
-INSERT INTO Escola (Nome, Codigo_Diretor, Codigo_Cidade) VALUES
+INSERT INTO escola (nome, codigo_diretor, codigo_cidade) VALUES
     ('Escola RIO', 4, 2),
     ('Escola VIDA', 8, 1);
 
-INSERT INTO Disciplina (Nome) VALUES
+INSERT INTO disciplina (nome) VALUES
     ('Matemática'),
     ('Português'),
     ('História'),
     ('Geografia');
 
-INSERT INTO Turma (Nome, Codigo_Escola) VALUES
+INSERT INTO turma (nome, codigo_escola) VALUES
     -- Escola RIO
     ('Turma 1A', 1),
     -- Escola VIDA
     ('Turma 1A', 2);
 
-INSERT INTO Aluno (Codigo, Matricula_Aluno, Data_Nascimento, Codigo_Turma) VALUES
+INSERT INTO aluno (codigo, matricula_aluno, data_nascimento, codigo_turma) VALUES
     -- Alunos Escola RIO
         -- turma 1A
     (3, 1001, '2010-03-24', 1),
@@ -55,18 +55,18 @@ INSERT INTO Aluno (Codigo, Matricula_Aluno, Data_Nascimento, Codigo_Turma) VALUE
     (1, 1003, '2010-09-10', 2),
     (7, 1004, '2010-01-24', 2);
 
-INSERT INTO Contato (Nome, Telefone, Codigo_Aluno) VALUES
+INSERT INTO contato (nome, telefone, codigo_aluno) VALUES
     ('Pai Arthur Medeiros', '(11) 9999-7777', 6),
     ('Mãe Maria Souza', '(21) 9888-6666', 3),
     ('Pai João Silva', '(11) 3456-1234', 1),
     ('Mãe Carlos Oliveira', '(15) 3458-2345', 7);
 
-INSERT INTO Ministra (Codigo_Professor, Codigo_Disciplina) VALUES
+INSERT INTO ministra (codigo_professor, codigo_disciplina) VALUES
     -- Ministra matemática
     (5, 1),
     (9, 1);
 
-INSERT INTO Dar_Aula (Codigo_Professor, Codigo_Disciplina, Codigo_Turma) VALUES
+INSERT INTO dar_aula (codigo_professor, codigo_disciplina, codigo_turma) VALUES
     -- Da aula na ESCOLA RIO em TURMA 1A
     (5, 1, 1),
     -- Da aula na ESCOLA VIDA em TURMA 1A
